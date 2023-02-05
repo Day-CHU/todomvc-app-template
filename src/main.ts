c670afd8adbf9c2039a2c73cee26c2b15573ce79
DayCHU <herooftime711@gmail.com>
2023-02-04 16:28:44 -1000

    new deployment

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
-
-const app = createApp(App);
-const store = createPinia();
-
-app.use(store);
-app.mount("#app");
